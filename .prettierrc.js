6414c45666bf82a329287b9e5fbce1c03d77f9aa
rajeshd <rajeshd@geekyants.com>
2022-06-18 15:56:20 +0530

    Commit with bare minimal remix setup

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  bracketSpacing: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  semi: true,
+  arrowParens: 'avoid',
+};
--- app/root.tsx
+++ app/root.tsx
@@ -1,52 +1,14 @@
-import type {
-  LinksFunction,
-  LoaderFunction,
-  MetaFunction,
-} from "@remix-run/node";
-import { json } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-
-import tailwindStylesheetUrl from "./styles/tailwind.css";
-import { getUser } from "./session.server";
-
-export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
-};
-
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "Remix Notes",
-  viewport: "width=device-width,initial-scale=1",
-});
-
-type LoaderData = {
-  user: Awaited<ReturnType<typeof getUser>>;
-};
-
-export const loader: LoaderFunction = async ({ request }) => {
-  return json<LoaderData>({
-    user: await getUser(request),
-  });
-};
+import {LiveReload, Outlet} from '@remix-run/react';
 
 export default function App() {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en">
       <head>
-        <Meta />
-        <Links />
+        <meta charSet="utf-8" />
+        <title>Remix: So great, it's funny!</title>
       </head>
-      <body className="h-full">
+      <body>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
         <LiveReload />
       </body>
     </html>
