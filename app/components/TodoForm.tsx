2c75f9a5abfdf25eac09542a23a51fd970311bb4
rajeshd <rajeshd@geekyants.com>
2022-06-24 12:00:15 +0530

    reseting form once adding is done

--- app/components/TodoForm.tsx
+++ app/components/TodoForm.tsx
@@ -1,13 +1,23 @@
-import type {ActionFunction} from '@remix-run/node';
-import {json, redirect} from '@remix-run/node';
-import {Form, useActionData} from '@remix-run/react';
-import {useEffect} from 'react';
-import {createNote} from '~/models/note.server';
+import {createRef, useEffect} from 'react';
+import {Form, useTransition} from '@remix-run/react';
 
 export default function TodoForm() {
+  const transition = useTransition();
+  const formRef = createRef<HTMLFormElement>();
+
+  const isAdding =
+    transition.state === 'submitting' &&
+    transition.submission.formData.get('_action') === 'create';
+
+  useEffect(() => {
+    if (!isAdding) {
+      formRef.current?.reset();
+    }
+  }, [isAdding]);
+
   return (
     <div>
-      <Form method="post">
+      <Form ref={formRef} method="post">
         <label>
           Add your item
           <input name="title" type="text" />
