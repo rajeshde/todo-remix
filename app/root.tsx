b0bb65964e75499fc9f312d0efa6cee068e77ee7
rajeshd <rajeshd@geekyants.com>
2022-06-24 11:20:18 +0530

    Scripts tag is added to allow JS to run

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import {LiveReload, Outlet} from '@remix-run/react';
+import {LiveReload, Outlet, Scripts} from '@remix-run/react';
 
 export default function App() {
   return (
@@ -10,6 +10,7 @@ export default function App() {
       <body>
         <Outlet />
         <LiveReload />
+        <Scripts />
       </body>
     </html>
   );
