c0f480aed3b6d4901e677f4f286a2c9f140b7859
rajeshd <rajeshd@geekyants.com>
2022-07-12 00:06:37 +0530

    eslint, prettier and tailwind packages are installed and configured properly

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,9 @@
-import {LiveReload, Outlet, Scripts} from '@remix-run/react';
+import type {LinksFunction} from '@remix-run/node';
+import {Links, LiveReload, Outlet, Scripts} from '@remix-run/react';
+
+import styles from './tailwind.css';
+
+export const links: LinksFunction = () => [{rel: 'stylesheet', href: styles}];
 
 export default function App() {
   return (
@@ -6,6 +11,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
         <Outlet />
