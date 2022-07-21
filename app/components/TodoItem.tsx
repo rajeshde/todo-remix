0a256f2896a1d003ef0ac635b9a28909a370caf9
rajeshd <rajeshd@geekyants.com>
2022-07-21 12:21:51 +0530

    TodoItem is created to handle edit, delete and update actions

--- app/components/TodoItem.tsx
+++ app/components/TodoItem.tsx
@@ -1,15 +1,50 @@
-import {Note} from '@prisma/client';
-import {Form} from '@remix-run/react';
+import {useRef} from 'react';
+import type {Note} from '@prisma/client';
+import {Form, Link} from '@remix-run/react';
 
 export default function TodoItem({item}: {item: Note}) {
+  const ref = useRef<HTMLFormElement>(null);
+
+  const handleOnChange = () => {
+    ref.current?.submit();
+  };
+
   return (
-    <li>
-      <Form method="post">
-        <span>{item.title}</span>
-        <input type="hidden" name="id" value={item.id} />
-        <button name="_action" value="delete">
-          X
-        </button>
+    <li className="border-1 my-2 rounded-md border border-indigo-200">
+      <Form ref={ref} method="post">
+        <input type="hidden" name="item" value={JSON.stringify(item)} />
+        <div className="flex h-10 items-center">
+          <div className="flex-auto">
+            <input
+              type="checkbox"
+              name="_action"
+              value="update"
+              checked={item.isCompleted}
+              onChange={handleOnChange}
+              className="mx-2 cursor-pointer"
+            />
+            <span
+              className={`${
+                item.isCompleted ? 'line-through' : 'no-underline'
+              }`}
+            >
+              {item.title}
+            </span>
+          </div>
+          <Link
+            to={`/edit/${item.id}`}
+            className="px-4 py-3 text-base font-medium text-blue-700"
+          >
+            Edit
+          </Link>
+          <button
+            name="_action"
+            value="delete"
+            className="mx-2 flex-none text-red-400"
+          >
+            Delete
+          </button>
+        </div>
       </Form>
     </li>
   );
