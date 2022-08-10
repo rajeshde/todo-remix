34d126782aef965db39936d7c0f380f2cadc8513
rajeshd <rajeshd422@gmail.com>
2022-08-10 15:21:14 +0530

    design fixes

--- app/components/TodoItem.tsx
+++ app/components/TodoItem.tsx
@@ -13,28 +13,30 @@ export default function TodoItem({item}: {item: Note}) {
     <li className="border-1 my-2 rounded-md border border-indigo-200">
       <Form ref={ref} method="post">
         <input type="hidden" name="item" value={JSON.stringify(item)} />
-        <div className="flex h-10 items-center">
-          <div className="flex-auto">
+        <div className="flex h-12 items-center">
+          <div className="flex-auto flex-row items-center">
             <input
               type="hidden"
               name="_action"
               value={item.isCompleted ? 'uncheck' : 'complete'}
             />
-            <input
-              type="checkbox"
-              name="_action"
-              value={item.isCompleted ? 'uncheck' : 'complete'}
-              checked={item.isCompleted}
-              onChange={handleOnChange}
-              className="mx-2 cursor-pointer"
-            />
-            <span
-              className={`${
-                item.isCompleted ? 'line-through' : 'no-underline'
-              }`}
-            >
-              {item.title}
-            </span>
+            <label className="block h-4 leading-4">
+              <input
+                type="checkbox"
+                name="_action"
+                value={item.isCompleted ? 'uncheck' : 'complete'}
+                checked={item.isCompleted}
+                onChange={handleOnChange}
+                className="mx-4 h-4 w-4 cursor-pointer align-bottom"
+              />
+              <span
+                className={`${
+                  item.isCompleted ? 'line-through' : 'no-underline'
+                } hover:cursor-pointer`}
+              >
+                {item.title}
+              </span>
+            </label>
           </div>
           <Link
             to={`/edit/${item.id}`}
@@ -45,7 +47,7 @@ export default function TodoItem({item}: {item: Note}) {
           <button
             name="_action"
             value="delete"
-            className="mx-2 flex-none text-red-400"
+            className="mx-2 flex-none text-red-600"
           >
             Delete
           </button>
